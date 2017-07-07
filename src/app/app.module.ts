3b1fe053aea9c2a7cc7355a2b0248a8dc70eb8e4
Volcomix <volcomix@gmail.com>
2017-07-07 20:58:52 +0200

    Simplify component hierarchy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeekComponent } from './week.component';
 import { DayComponent } from './day.component';
-import { HourComponent } from './hour.component';
-import { HourLabelComponent } from './hour-label.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WeekComponent,
-    DayComponent,
-    HourComponent,
-    HourLabelComponent
+    DayComponent
   ],
   imports: [
     BrowserModule,
--- src/app/hour.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'scheduler-hour',
-  templateUrl: './hour.component.html',
-  styleUrls: ['./hour.component.scss']
-})
-export class HourComponent {
-}
