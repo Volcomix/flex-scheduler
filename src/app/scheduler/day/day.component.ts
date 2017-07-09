184d9427ada289ae304c35e6e573bf618d50cc32
Volcomix <volcomix@gmail.com>
2017-07-09 20:39:05 +0200

    Move events through days of week

--- src/app/scheduler/day/day.component.ts
+++ src/app/scheduler/day/day.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  ElementRef,
-  Input,
-  HostListener
-} from '@angular/core';
-
-import * as moment from 'moment';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 import { Event } from '../event.model';
 
@@ -19,136 +12,19 @@ export class DayComponent {
   @Input() hours: number[];
   @Input() events: Event[];
   @Input() step: number;
-
-  resizingEvent: Event;
-  movingEvent: Event;
-
-  private el: HTMLElement;
-  private startDate: Date;
-  private moveOffset: number;
-
-  constructor(el: ElementRef) {
-    this.el = el.nativeElement;
-  }
+  @Output() resizeEvent = new EventEmitter<Event>();
+  @Output() moveEvent = new EventEmitter<MovingEvent>();
 
   startResizingEvent(event: Event) {
-    this.resizingEvent = event;
+    this.resizeEvent.emit(event);
   }
 
   startMovingEvent(event: Event, offset: number) {
-    this.movingEvent = event;
-    this.moveOffset = offset;
+    this.moveEvent.emit({ event, offset });
   }
+}
 
-  @HostListener('mousedown', ['$event'])
-  onMouseDown(mouseEvent: MouseEvent) {
-    if (mouseEvent.button !== 0) {
-      return;
-    }
-    this.createEvent(mouseEvent.clientY);
-  }
-
-  @HostListener('touchstart', ['$event'])
-  onTouchStart(touchEvent: TouchEvent) {
-    touchEvent.preventDefault();
-    this.createEvent(touchEvent.touches[0].clientY);
-  }
-
-  private createEvent(clientY: number) {
-    this.startDate = this.getDate(clientY);
-
-    // Start hour must not be 24h
-    if (this.startDate.getDate() > this.day.getDate()) {
-      this.startDate = moment(this.startDate)
-        .subtract(this.step, 'minutes')
-        .toDate();
-    }
-
-    const endDate = moment(this.startDate)
-      .add(this.step, 'minutes')
-      .toDate();
-    this.resizingEvent = new Event(this.startDate, endDate);
-    this.events.push(this.resizingEvent);
-  }
-
-  @HostListener('document:mousemove', ['$event'])
-  onMouseMove(mouseEvent: MouseEvent) {
-    if (mouseEvent.button !== 0) {
-      return;
-    }
-    mouseEvent.preventDefault();
-    this.updateEvent(mouseEvent.clientY);
-  }
-
-  @HostListener('document:touchmove', ['$event'])
-  onTouchMove(touchEvent: TouchEvent) {
-    this.updateEvent(touchEvent.touches[0].clientY);
-  }
-
-  private updateEvent(clientY: number) {
-    if (this.resizingEvent) {
-      this.resizeEvent(clientY);
-    } else if (this.movingEvent) {
-      this.moveEvent(clientY);
-    }
-  }
-
-  private resizeEvent(clientY: number) {
-    const date = this.getDate(clientY);
-    if (this.startDate && date <= this.startDate) {
-      this.resizingEvent.startDate = date;
-    } else if (this.startDate || date > this.resizingEvent.startDate) {
-      this.resizingEvent.endDate = date;
-    }
-  }
-
-  private moveEvent(clientY: number) {
-    if (!this.movingEvent) {
-      return;
-    }
-    const diff = moment(this.movingEvent.endDate)
-      .diff(this.movingEvent.startDate, 'minutes');
-    let startDate = this.getDate(clientY - this.moveOffset);
-    let endDate = moment(startDate)
-      .add(diff, 'minutes')
-      .toDate();
-    if (endDate.getDate() > this.day.getDate()) {
-      endDate = moment(this.day)
-        .add(1, 'day')
-        .toDate();
-      startDate = moment(endDate)
-        .subtract(diff, 'minutes')
-        .toDate();
-    }
-    this.movingEvent.startDate = startDate;
-    this.movingEvent.endDate = endDate;
-  }
-
-  @HostListener('document:mouseup')
-  @HostListener('document:touchend')
-  onMoveEnd() {
-    if (this.resizingEvent) {
-      this.resizingEvent = undefined;
-      this.startDate = undefined;
-    } else if (this.movingEvent) {
-      this.movingEvent = undefined;
-      this.moveOffset = undefined;
-    }
-  }
-
-  private getDate(clientY: number) {
-    const rect = this.el.getBoundingClientRect();
-    const max = rect.bottom - rect.top;
-    const y = clientY - rect.top;
-    let minutes = Event.minutesPerDay * y / max;
-    minutes = Math.round(minutes / this.step) * this.step;
-    if (minutes < 0) {
-      minutes = 0;
-    } else if (minutes > Event.minutesPerDay) {
-      minutes = Event.minutesPerDay;
-    }
-    return moment(this.day)
-      .minutes(minutes)
-      .toDate();
-  }
+export class MovingEvent {
+  offset: number;
+  event: Event;
 }
--- src/app/scheduler/week/week.component.ts
+++ src/app/scheduler/week/week.component.ts
@@ -1,9 +1,19 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  HostListener,
+  HostBinding,
+} from '@angular/core';
 
 import * as moment from 'moment';
 
 import { Events } from '../events.model';
+import { Event } from '../event.model';
 import { Day } from '../day.model';
+import { MovingEvent } from '../day/day.component';
 
 @Component({
   selector: 'scheduler-week',
@@ -18,14 +28,22 @@ export class WeekComponent implements OnChanges {
   days: Day[];
   hours: number[];
 
-  constructor() {
+  resizingEvent: Event;
+  movingEvent: Event;
+
+  private el: HTMLElement;
+  private startDate: Date;
+  private moveOffset: number;
+
+  constructor(el: ElementRef) {
+    this.el = el.nativeElement;
     this.hours = [];
     for (let i = 0; i < 24; i++) {
       this.hours.push(i);
     }
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
+  ngOnChanges(changes: SimpleChanges) {
     const date: Date = changes.date.currentValue;
     const events: Events = changes.events.currentValue;
     const hasDateChanged = date !== changes.date.previousValue;
@@ -33,6 +51,10 @@ export class WeekComponent implements OnChanges {
     if (!hasDateChanged && !hasEventsChanged) {
       return;
     }
+    this.initDays(date, events);
+  }
+
+  private initDays(date: Date, events: Events) {
     this.days = [];
     for (let i = 0; i < 7; i++) {
       const day = moment(date)
@@ -45,5 +67,172 @@ export class WeekComponent implements OnChanges {
         events: events[+day]
       });
     }
+  }
+
+  @HostBinding('class.resizing') get isResizingEvent(): boolean {
+    return this.resizingEvent !== undefined;
+  }
+
+  @HostBinding('class.movingEvent') get isMoveingEvent(): boolean {
+    return this.movingEvent !== undefined;
+  }
+
+  startResizingEvent(event: Event) {
+    this.resizingEvent = event;
+  }
+
+  startMovingEvent(movingEvent: MovingEvent) {
+    this.movingEvent = movingEvent.event;
+    this.moveOffset = movingEvent.offset;
+  }
+
+  @HostListener('mousedown', ['$event'])
+  onMouseDown(mouseEvent: MouseEvent) {
+    if (mouseEvent.button !== 0) {
+      return;
+    }
+    this.createEvent(mouseEvent);
+  }
+
+  @HostListener('touchstart', ['$event'])
+  onTouchStart(touchEvent: TouchEvent) {
+    touchEvent.preventDefault();
+    this.createEvent(touchEvent.touches[0]);
+  }
+
+  private createEvent({ clientX, clientY }: {
+    clientX: number,
+    clientY: number
+  }) {
+    const day = this.getDay(clientX);
+    this.startDate = this.getDate(day.date, clientY);
+
+    // Start hour must not be 24h
+    if (this.startDate.getDate() > day.date.getDate()) {
+      this.startDate = moment(this.startDate)
+        .subtract(this.step, 'minutes')
+        .toDate();
+    }
+
+    const endDate = moment(this.startDate)
+      .add(this.step, 'minutes')
+      .toDate();
+    this.resizingEvent = new Event(this.startDate, endDate);
+    day.events.push(this.resizingEvent);
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(mouseEvent: MouseEvent) {
+    if (mouseEvent.button !== 0) {
+      return;
+    }
+    mouseEvent.preventDefault();
+    this.updateEvent(mouseEvent);
+  }
+
+  @HostListener('document:touchmove', ['$event'])
+  onTouchMove(touchEvent: TouchEvent) {
+    this.updateEvent(touchEvent.touches[0]);
+  }
+
+  private updateEvent(uiEvent: MouseEvent | Touch) {
+    if (this.resizingEvent) {
+      this.resizeEvent(uiEvent);
+    } else if (this.movingEvent) {
+      this.moveEvent(uiEvent);
+    }
+  }
+
+  private resizeEvent({ clientX, clientY }: {
+    clientX: number,
+    clientY: number
+  }) {
+    const day = moment(this.resizingEvent.startDate)
+      .startOf('day')
+      .toDate();
+    const date = this.getDate(day, clientY);
+    if (this.startDate && date <= this.startDate) {
+      this.resizingEvent.startDate = date;
+    } else if (this.startDate || date > this.resizingEvent.startDate) {
+      this.resizingEvent.endDate = date;
+    }
+  }
+
+  private moveEvent({ clientX, clientY }: {
+    clientX: number,
+    clientY: number
+  }) {
+    if (!this.movingEvent) {
+      return;
+    }
+    const diff = moment(this.movingEvent.endDate)
+      .diff(this.movingEvent.startDate, 'minutes');
+    const day = this.getDay(clientX);
+    let startDate = this.getDate(day.date, clientY - this.moveOffset);
+    let endDate = moment(startDate)
+      .add(diff, 'minutes')
+      .toDate();
+    if (endDate.getDate() > day.date.getDate()) {
+      endDate = moment(day.date)
+        .add(1, 'day')
+        .toDate();
+      startDate = moment(endDate)
+        .subtract(diff, 'minutes')
+        .toDate();
+    }
+    if (startDate.getDate() !== this.movingEvent.startDate.getDate()) {
+      this.moveEventToDay(day);
+    }
+    this.movingEvent.startDate = startDate;
+    this.movingEvent.endDate = endDate;
+  }
+
+  @HostListener('document:mouseup')
+  @HostListener('document:touchend')
+  onMoveEnd() {
+    if (this.resizingEvent) {
+      this.resizingEvent = undefined;
+      this.startDate = undefined;
+    } else if (this.movingEvent) {
+      this.movingEvent = undefined;
+      this.moveOffset = undefined;
+    }
+  }
+
+  private moveEventToDay(day: Day) {
+    const previousDayIdx = moment(this.movingEvent.startDate).weekday();
+    const previousDay = this.days[previousDayIdx];
+    const eventIdx = previousDay.events.indexOf(this.movingEvent);
+    this.days[previousDayIdx].events.splice(eventIdx, 1);
+    day.events.push(this.movingEvent);
+  }
+
+  private getDay(clientX: number): Day {
+    const rect = this.el.getBoundingClientRect();
+    const max = rect.right - rect.left;
+    const x = clientX - rect.left;
+    let idx = Math.floor(7 * x / max);
+    if (idx < 0) {
+      idx = 0;
+    } else if (idx > 6) {
+      idx = 6;
+    }
+    return this.days[idx];
+  }
+
+  private getDate(day: Date, clientY: number): Date {
+    const rect = this.el.getBoundingClientRect();
+    const max = rect.bottom - rect.top;
+    const y = clientY - rect.top;
+    let minutes = Event.minutesPerDay * y / max;
+    minutes = Math.round(minutes / this.step) * this.step;
+    if (minutes < 0) {
+      minutes = 0;
+    } else if (minutes > Event.minutesPerDay) {
+      minutes = Event.minutesPerDay;
+    }
+    return moment(day)
+      .minutes(minutes)
+      .toDate();
   }
 }
